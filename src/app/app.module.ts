c0c358c2722fa056040ba13c06310feb05cd98c0
Shivam Yadav <sky.java9@gmail.com>
2018-11-09 21:33:26 +0530

    Upload project to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatToolbarModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { ImgCardComponent } from './img-card/img-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
